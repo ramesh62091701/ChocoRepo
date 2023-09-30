1abd753f0574ce182d0b6b0fcd72f418db0d2eff
Rajesh_Sonata <rajesh.subramanian@sonata-software.com>
2023-09-30 12:25:36 +0530

    changes to update all the report into consolidate one file in one sheet.

--- LegacyExplorer/code/LegacyExplorer.ConsoleApp/TestProgram3.cs
+++ LegacyExplorer/code/LegacyExplorer.ConsoleApp/TestProgram3.cs
@@ -8,6 +8,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using System.Configuration;
+using System.Collections.Specialized;
 
 namespace LegacyExplorer.ConsoleApp
 {
@@ -15,6 +19,10 @@ namespace LegacyExplorer.ConsoleApp
     {
         public static void Test(string[] args)
         {
+
+            NameValueCollection appSettings = ConfigurationManager.AppSettings;
+            string isOutputSingleFile  = appSettings["OutputSingleFile"];
+
             List<string> listAssemnlyPath = new List<string>();
             listAssemnlyPath.Add("D:\\Downloads\\BlogEngine.NET-master\\BlogEngine.NET-master\\BlogEngine\\BlogEngine.NET\\bin\\BlogEngine.NET.dll");
             listAssemnlyPath.Add("D:\\Downloads\\BlogEngine.NET-master\\BlogEngine.NET-master\\BlogEngine\\BlogEngine.NET\\bin\\BlogEngine.Core.dll");
@@ -31,15 +39,30 @@ namespace LegacyExplorer.ConsoleApp
             var csvExporter = new CsvExporter("D:\\rnd\\output");
 
             Console.WriteLine($"Writing output to location {csvExporter.OutputDirectory}");
+            if (isOutputSingleFile == "true")
+            {
+                List<dynamic> dynamics = new List<dynamic>();
+                dynamics.Add(output.Assemblies);
+                dynamics.Add(output.References);
+                dynamics.Add(output.Types);
+                dynamics.Add(output.Fields);
+                dynamics.Add(output.Properties);
+                dynamics.Add(output.Methods);
+                dynamics.Add(output.BaseClasses);
 
-            csvExporter.ExportToCsv<NetAssembly>(output.Assemblies);
-            csvExporter.ExportToCsv<NetReference>(output.References);
-            csvExporter.ExportToCsv<NetType>(output.Types);
-            csvExporter.ExportToCsv<NetField>(output.Fields);
-            csvExporter.ExportToCsv<NetProperty>(output.Properties);
-            csvExporter.ExportToCsv<NetMethod>(output.Methods);
-            csvExporter.ExportToCsv<NetBaseClass>(output.BaseClasses);
 
+                csvExporter.ExportToCsv(dynamics, true);
+            }
+            else
+            {
+                csvExporter.ExportToCsv<NetAssembly>(output.Assemblies);
+                csvExporter.ExportToCsv<NetReference>(output.References);
+                csvExporter.ExportToCsv<NetType>(output.Types);
+                csvExporter.ExportToCsv<NetField>(output.Fields);
+                csvExporter.ExportToCsv<NetProperty>(output.Properties);
+                csvExporter.ExportToCsv<NetMethod>(output.Methods);
+                csvExporter.ExportToCsv<NetBaseClass>(output.BaseClasses);
+            }
 
             Console.WriteLine($"Export Complete...");
 
