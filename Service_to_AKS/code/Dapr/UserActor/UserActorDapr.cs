7fb58b411297ea950b840a90b39cb53fbfb7295f
Sharad <sharad.k@sonata-software.com>
2024-02-05 13:08:00 +0530

    Fixes

--- Service_to_AKS/code/Dapr/UserActor/UserActorDapr.cs
+++ Service_to_AKS/code/Dapr/UserActor/UserActorDapr.cs
@@ -1,4 +1,5 @@
 ﻿using Dapr.Actors.Runtime;
+using Shared;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,6 +32,37 @@ namespace UserActor
                 if (string.IsNullOrEmpty(productId)) continue;
                 await StateManager.RemoveStateAsync(productId);
             }
+        }
+
+        public async Task<BasketItem[]> GetBasket()
+        {
+            var result = new List<BasketItem>();
+
+            var products = await StateManager.GetStateAsync<string>(StateNames);
+
+            foreach (string productId in products.Split(","))
+            {
+                if (string.IsNullOrEmpty(productId)) continue;
+                int quantity = await StateManager.GetStateAsync<int>(productId);
+                result.Add(
+                   new BasketItem
+                   {
+                       ProductId = new Guid(productId),
+                       Quantity = quantity
+                   });
+            }
+
+            return result.ToArray();
+        }
+
+        /// <summary>
+        /// This method is called whenever an actor is activated.
+        /// An actor is activated the first time any of its methods are invoked.
+        /// </summary>
+        protected override Task OnActivateAsync()
+        {
+            ActorEventSource.Current.ActorMessage(this.Id.ToString(), "Actor activated.");
+            return Task.FromResult(true);
         }
     }
 }
