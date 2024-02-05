a3d80731029c423efb4bc2110506ca3ad62a72c2
m.abhishek@sonata-software.com <m.abhishek@sonata-software.com>
2024-02-05 12:40:19 +0530

    service, dapr

---
+++ Service_to_AKS/code/Dapr/UserActor/UserActorDapr.cs
@@ -0,0 +1,33 @@
+﻿using Dapr.Actors.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserActor
+{
+    internal class UserActorDapr : Actor
+    {
+        public UserActorDapr(ActorHost host) : base(host)
+        {
+        }
+
+        public async Task AddToBasket(Guid productId, int quantity)
+        {
+            await StateManager.AddOrUpdateStateAsync(productId.ToString(),
+               quantity,
+               (id, oldQuantity) => oldQuantity + quantity);
+        }
+
+        public async Task ClearBasket()
+        {
+            IEnumerable<string> productIDs = await StateManager.GetStateAsync();
+
+            foreach (string productId in productIDs)
+            {
+                await StateManager.RemoveStateAsync(productId);
+            }
+        }
+    }
+}
