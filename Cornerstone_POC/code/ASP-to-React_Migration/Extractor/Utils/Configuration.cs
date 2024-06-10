bfa7196bfb3c6a6535df395ebac0b3f6a0507c53
Abhishek M <m.abhishek@sonata-software.com>
2024-06-10 20:47:30 +0530

    commit

--- Cornerstone_POC/code/ASP-to-React_Migration/Extractor/Utils/Configuration.cs
+++ Cornerstone_POC/code/ASP-to-React_Migration/Extractor/Utils/Configuration.cs
@@ -10,8 +10,8 @@ namespace Extractor.Utils
     {
         public const string GptApiKey = "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";
         public const string GptUrl = "https://api.openai.com/v1/chat/completions";
-        //public const string FigmaToken = "figd_SnGSVtDwY72khK4UaD1WjM4DQKjrLG9nfDdCTZgh"; //For Testing
-        public const string FigmaToken = "figd_9UebKiPA_se3rlzpDh51A1x8gO7JTBxawUZvL_Dz"; //CSOD Figma design token
+        public const string FigmaToken = "figd_SnGSVtDwY72khK4UaD1WjM4DQKjrLG9nfDdCTZgh"; //For Testing
+        //public const string FigmaToken = "figd_9UebKiPA_se3rlzpDh51A1x8gO7JTBxawUZvL_Dz"; //CSOD Figma design token
 
     }
 }
